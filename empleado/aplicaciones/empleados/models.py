f1f522f42a3ff54627a7c672fb0414c26da3bd66
David Carrillo <david219@ciencias.unam.mx>
2024-05-12 20:16:15 -0600

    Agregando migraciones

--- empleado/aplicaciones/empleados/models.py
+++ empleado/aplicaciones/empleados/models.py
@@ -14,10 +14,12 @@ class Empleado(models.Model):
     )
 
     first_name = models.CharField('Nombre',max_length=20,null=False)
-    last_name = models.CharField('Apellidos', max_length=30, null=False)
+    last_name  = models.CharField('Apellidos', max_length=30, null=False)
     job = models.CharField('Trabajo', max_length=1,choices=JOB_CHOICES)
-    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE, on_update=models.CASCADE)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     #image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
 
 
+    def __str__(self):
+        return self.id + '-' + self.first_name + '-' + self.last_name + '-' + self.job + '-' + self.departamento
     
